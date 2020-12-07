092e86d7adcaac69e7ac5c2e2d22122abf1b1e2c
Dzmitry Kushniaruk <xdzmkus@gmail.com>
2020-12-08 00:46:36 +0300

    flame updated

--- src/FlameLedEffect.cpp
+++ src/FlameLedEffect.cpp
@@ -30,13 +30,6 @@ bool FlameLedEffect::paint()
 	if (!LedEffect::isReady())
 		return false;
 
-	// SPARKING: What chance (out of 255) is there that a new spark will be lit?
-	// Higher chance = more roaring fire.  Lower chance = more flickery fire.
-	// Default 120, suggested range 50-200.
-	const uint8_t SPARKING = 120;
-
-	random16_add_entropy(random16());
-
 	// Step 1.  Cool down every cell a little
 	for (uint16_t i = 0; i < numLeds; i++)
 	{
@@ -50,20 +43,19 @@ bool FlameLedEffect::paint()
 	}
 
 	// Step 3.  Randomly ignite new 'sparks' of heatMap near the bottom
-	if (random8() < SPARKING)
+	// What chance (out of 255) is there that a new spark will be lit?
+	if (random8() < 64)
 	{
-		uint8_t y = random8(numLeds>>2);
+		uint8_t y = random8(max(3, numLeds/4));
 		heatMap[y] = qadd8(heatMap[y], random8(160, 255));
 	}
 
 	// Step 4.  Map from heatMap cells to LED colors
 	for (uint16_t j = 0; j < numLeds; j++)
 	{
-		// Scale the heatMap value from 0-255 down to 0-240
-		// for best results with color palettes.
-		uint8_t colorindex = scale8(heatMap[j], 240);
-		CRGB color = ColorFromPalette(gPal, colorindex);
-		uint16_t pixelnumber = fireReversed ? (numLeds - 1) - j : j;
+		// Scale the heatMap value from 0-255 down to 0-240 for best results with color palettes.
+		CRGB color = ColorFromPalette(gPal, scale8(heatMap[j], 240));
+		uint16_t pixelnumber = fireReversed ? (numLeds - 1 - j) : j;
 		ledLine[pixelnumber] = color;
 	}
 
